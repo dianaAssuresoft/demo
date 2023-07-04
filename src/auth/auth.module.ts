2fd0686132d8ce87093efac44603160674a0416a
HaMGPO <hamed.pinto@assuresoftbolivia.com>
2023-07-04 17:59:20 -0400

    activedirectory2

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,17 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt'
-import { jwtConstants } from './constants';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { PassportModule } from '@nestjs/passport';
-import { ADStrategy } from './strategies/activedirectory.strategy';
 
 @Module({
   imports: [
     UsersModule,
-    PassportModule,
     TypeOrmModule.forFeature([
       User
     ]), 
@@ -23,7 +18,7 @@ import { ADStrategy } from './strategies/activedirectory.strategy';
     // }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, ADStrategy],
-  exports: [TypeOrmModule, ADStrategy, PassportModule]
+  providers: [AuthService],
+  exports: [TypeOrmModule]
 })
 export class AuthModule {}
